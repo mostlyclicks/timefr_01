0fe25b5595f6e66b7fe8ae387b7ed605143e341f
Carlos Interone <carlos@mostlyclicks.com>
2015-04-06 09:22:19 -0500

    get easing scrollbtn and logo working

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,21 @@
+
+/* ==============================================
+Smooth Scroll To Anchor
+=============================================== */
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.navbar a,.btn,.scroll-btn').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 105
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+//STICKY NAV
+$(document).ready(function(){
+    $(".navbar").sticky({topSpacing:0});
+});
