2ecb59116c4d78815a6ad10654aa1defd1bd74e1
Carlos Interone <carlos@mostlyclicks.com>
2015-04-13 13:29:42 -0500

    test push after renaming

--- db/migrate/20150409155305_add_bike_id_to_colors.rb
+++ db/migrate/20150409155305_add_bike_id_to_colors.rb
@@ -1,5 +1,5 @@
-class AddBikeIdToColors < ActiveRecord::Migration
-  def change
-    #add_columm :refinery_bikes_colors, :bike_id, :integer
-  end
-end
+# class AddBikeIdToColors < ActiveRecord::Migration
+#   def change
+#     #add_columm :refinery_bikes_colors, :bike_id, :integer
+#   end
+# end
