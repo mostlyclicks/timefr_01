29613738190ce434bd2a736e1fb763323a30ece0
Carlos Interone <carlos@mostlyclicks.com>
2015-04-14 06:38:56 -0500

    enable s3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :load_home_features
+  before_filter :load_home_features, :load_home_skylons
 
   protected
 
   def load_home_features
     @home_features = Refinery::HomeFeatures::HomeFeature.all
   end
+
+  def load_home_skylons
+    # @skylons = Refinery::Bikes::Bike.first.colors.all
+    # @skylons.each do |s|
+    #   if s.name =~
+    # end
+
+
+    @home_skylon_aktiv = Refinery::Bikes::Bike.first.colors.where(name: "Skylon AKTIV | Team")
+    @home_skylon = Refinery::Bikes::Bike.first.colors.where(name: "Skylon | White")
+    @bike_type = Refinery::Bikes::Bike.first.bike_type
+  end
+
+  # def load_home_izons
+  #   @home_
+  # end
 end
