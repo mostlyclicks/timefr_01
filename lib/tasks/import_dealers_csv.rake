8d464a1ab177599da37c4904e4cfd548b0f92d30
Carlos Interone <carlos@mostlyclicks.com>
2015-09-22 12:06:41 -0500

    add us dealers csv and adjust rake

--- lib/tasks/import_dealers_csv.rake
+++ lib/tasks/import_dealers_csv.rake
@@ -1,10 +1,13 @@
 require 'csv'
 namespace :import_dealers_csv do
   task :create_dealers => :environment do
-    csv_text = File.read('doc/2015_FR_dealer.csv')
+    csv_text = File.read('doc/2015_US_dealer.csv')
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
       Refinery::Dealers::Dealer.create!(row.to_hash)
     end
   end
 end
+
+# rake task--
+# heroku run rake import_dealers_csv:create_dealers
--- vendor/extensions/dealers/app/models/refinery/dealers/dealer.rb
+++ vendor/extensions/dealers/app/models/refinery/dealers/dealer.rb
@@ -6,7 +6,7 @@ module Refinery
       attr_accessible :account_number, :dealer_name, :street_address_1, :street_address_2, :postal_code, :city, :telephone_1, :state_province, :country, :email, :website, :position
 
       #validates :dealer_name, :presence => true, :uniqueness => true
-      validates :account_number, :presence => true, :uniqueness => true
+      #validates :account_number, :presence => true, :uniqueness => true
     end
   end
 end
