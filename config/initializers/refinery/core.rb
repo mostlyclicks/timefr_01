bcb4419c887f03ad0b070df8e46fb7be5ea02199
Carlos Interone <carlos@mostlyclicks.com>
2015-09-23 11:00:52 -0500

    adjust title, insert and comment out GA code for timesport.com

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -20,12 +20,12 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "TIME Sport INTERNATIONAL"
+  config.site_name = "TIME Sport - Carbon Fiber Bicycle Frames and Pedals - Handmade in France"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
   # Google Analytics are made.
-  # config.google_analytics_page_code = "UA-xxxxxx-x"
+  # config.google_analytics_page_code = "UA-21814715-1"
 
   # Enable/disable authenticity token on frontend
   # config.authenticity_token_on_frontend = false
