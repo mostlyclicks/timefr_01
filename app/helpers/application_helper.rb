6ba82b42fff3159536b804cbd95a35619445a692
Carlos Interone <carlos@mostlyclicks.com>
2016-09-27 10:02:13 -0600

    fix geometry picker for SCYLON

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
   def get_bike_geometry(bike)
     if bike == 'Skylon' 
       @bike_geometry = Refinery::Geometries::Geometry.where(name: 'Skylon')
+    elsif bike == 'SCYLON' 
+      @bike_geometry = Refinery::Geometries::Geometry.where(name: 'Skylon')
     elsif bike == 'Izon' 
       @bike_geometry = Refinery::Geometries::Geometry.where(name: 'Izon')
     else 
