ee4b69c60a278ced27684edd28f0eb11cd748d32
Carlos Interone <carlos@mostlyclicks.com>
2016-10-25 10:07:11 -0600

    add fade in JS, fig, figcaption to time-racing

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -28,6 +28,33 @@ $(function () {
 });
 
 
+//FADE IN
+
+$(document).ready(function() {
+  
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+  
+        /* Check the location of each desired element */
+        $('.hide-image').each( function(i){
+          
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+              
+                $(this).animate({'opacity':'1'},2000);
+                  
+            }
+          
+        }); 
+
+    });
+
+});
+
+
 
 //Parallax
 
