a2aff9ba1b6b1cb5db788b69a7d4002faaf4f17f
Carlos Interone <carlos@mostlyclicks.com>
2016-11-16 16:04:28 -0700

    set order in dealer admin - dealer_name ASC

--- vendor/extensions/dealers/app/controllers/refinery/dealers/admin/dealers_controller.rb
+++ vendor/extensions/dealers/app/controllers/refinery/dealers/admin/dealers_controller.rb
@@ -3,9 +3,19 @@ module Refinery
     module Admin
       class DealersController < ::Refinery::AdminController
 
+        #before_action :order_dealers
+
         crudify :'refinery/dealers/dealer',
                 :title_attribute => 'dealer_name',
+                order: "dealer_name ASC",
                 :xhr_paging => true
+
+
+        protected
+
+          def order_dealers
+
+          end
 
       end
     end
