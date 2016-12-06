d73ae1dcf6fe5a8c4ff708e11f8c668115aabc20
Carlos Interone <carlos@mostlyclicks.com>
2016-12-06 13:39:06 -0700

    take out css compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Timefr01::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
